102d14af0f6ba03cdf9af94217a0efe2141a2379
Olga Bovkun <olga.bovkun.ob@gmail.com>
2023-04-18 15:00:42 -0500

    send message and handle error/success

--- ms-producer/src/main/java/com/obovkun/springkafka/KafkaProducerApplication.java
+++ ms-producer/src/main/java/com/obovkun/springkafka/KafkaProducerApplication.java
@@ -2,7 +2,9 @@ package com.obovkun.springkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({ KafkaProducerProperties.class })
 @SpringBootApplication
 public class KafkaProducerApplication {
 
--- ms-producer/src/main/java/com/obovkun/springkafka/service/KafkaSender.java
+++ ms-producer/src/main/java/com/obovkun/springkafka/service/KafkaSender.java
@@ -1,22 +1,43 @@
 package com.obovkun.springkafka.service;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import com.obovkun.springkafka.KafkaProducerProperties;
 import com.obovkun.springkafka.dto.KafkaMessageDto;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class KafkaSender {
 
     private KafkaTemplate<String, String> kafkaTemplate;
     private KafkaProducerProperties producerProperties;
 
     public void sendMessage(KafkaMessageDto kafkaMessageDto) {
-        kafkaTemplate.send(producerProperties.getKafkaTopicStr(), kafkaMessageDto.message());
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(producerProperties.getKafkaTopicStr(),
+                kafkaMessageDto.message());
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                handleSuccess(result);
+            } else {
+                handleFailure(result, ex);
+            }
+        });
+    }
+
+    private void handleSuccess(SendResult<String, String> result) {
+        log.info("Message was sent successfully: {}", result);
+    }
+
+    private void handleFailure(SendResult<String, String> result, Throwable ex) {
+        log.error("Unable to send the message due to: {}", ex.getMessage());
     }
 
 }
